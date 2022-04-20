35b84f9d9b80a0d615aa977f8b87993b3048f087
vivekanandhan <vivekanandhan82@gmail.com>
2022-04-20 17:07:02 -0400

    new commit to poll scm

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World mody!" );
+        System.out.println( "Hello world-new commit by dev1!" );
     }
 }
